cd46bd550a318aad64ebd0ea1d22d21e467f2377
Surafel Aschalew <surafelaschalew18@gmail.com>
2025-08-27 09:40:48 +0200

    update footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -63,36 +63,36 @@ export default function Footer(){
         </div>
 
         <FooterAccordionItem
-          title="Join us"
+          title="Product"
           items={[
-            {name: "Careers", to: "join-us/careers"},
-            {name: "Lime Time Blog", to: "join-us/blog"},
-            {name: "Press", to: "join-us/press"},
-            {name: "Partners", to: "join-us/partners"}
+            {name: "How it works", to: "product/how-it-works"},
+            {name: "Pricing", to: "product/pricing"},
+            {name: "Guzo Locations", to: "product/guzo-locations"},
+            {name: "Safety", to: "product/safety"}
           ]}
         />
 
         <FooterAccordionItem
-          title="About"
+          title="Company"
           items={[
-            {name: "Community", to: "about/community"},
-            {name: "E-Bike", to: "about/e-bike"},
-            {name: "E-Scooter", to: "about/e-scooter"}, 
-            {name: "Sustainability", to: "about/sustainability"},
-            {name: "Innovation", to: "about/innovation"},
-            {name: "Safety", to: "about/safety"}
+            {name: "Community", to: "company/community"},
+            {name: "About us", to: "about-us"},
+            {name: "E-Bike", to: "company/e-bike"},
+            {name: "E-Scooter", to: "company/e-scooter"}, 
+            {name: "Sustainability", to: "company/sustainability"},
+            {name: "Innovation", to: "company/innovation"},
           ]}
         />
 
         <FooterAccordionItem
-          title="Programs"
+          title="Resources"
           items={[
-            {name: "Advertise", to: "programs/advertise"},
-            {name: "Lime Access", to: "programs/lime-access"},
-            {name: "Lime Hero", to: "programs/lime-hero"},
-            {name: "Lime Assist", to: "programs/lime-assist"},
-            {name: "Insurance", to: "programs/insurance"},
-            {name: "Our Cities", to: "programs/our-cities"}
+            {name: "Help Center", to: "help"},
+            {name: "FAQ's", to: "resources/faq"},
+            {name: "Support", to: "resources/support"},
+            {name: "Contact us", to: "resources/contact-us"},
+            {name: "Insurance", to: "resources/insurance"},
+            {name: "Our Cities", to: "resources/our-cities"}
           ]}
         />        
 
